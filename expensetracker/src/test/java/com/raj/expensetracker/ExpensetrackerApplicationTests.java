8ba151314211c44c1cda66d0a65ce3ffdfd80d7f
Raj Tiwari <rajtiwari16916@gmail.com>
2026-05-13 22:14:17 +0000

    Setup clean Spring Boot project structure

---
+++ expensetracker/src/test/java/com/raj/expensetracker/ExpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
